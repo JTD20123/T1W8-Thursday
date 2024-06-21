e3f81e2a260359362b02e981f70e60530345a1b7
JTD20123 <jasperdayuno@yahoo.com>
2024-06-22 09:16:37 +1000

    T1w8-thursday

---
+++ exception_handling.py
@@ -0,0 +1,9 @@
+def convert_to_integer(value):
+    result = int(value)
+    print(f'Conversion Successful! Result: {result}')
+
+# User input
+user_input = input("Enter a number to convert to integer: ")
+
+# Convert the number
+convert_to_integer(user_input)
---
+++ stack_trace.py
@@ -0,0 +1,25 @@
+def function_a():
+    print("Function A started")
+    function_b()
+    print("Function A ended")
+
+def function_b():
+    print("Function B started")
+    function_c()
+    print("Function B ended")
+
+def function_c():
+    print("Function C started")
+    # Intentional Errrdef function_a():
+    print("Function A started")
+    function_b()
+    print("Function A ended")
+
+def function_b():
+    print("Function B started")
+    function_c()
+    print("Function B ended")
+
+def function_c():
+    print("Function C started")
+    # Intentional Errr
